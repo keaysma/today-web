27f5ae76e46613dd4dc0ec57040ac069e3be4e35
Michael-Andrew Keays <michaelandrewkeays@gmail.com>
2024-08-04 14:48:54 -0400

    item consts

---
+++ constants/items.ts
@@ -0,0 +1,12 @@
+export const TYPES_WITH_CUSTOM_KEYS = [
+    "checkbox",
+    "checkbox-link"
+]
+
+export const TYPES_WITH_LINK_CONFIG = [
+    "checkbox"
+] // ["checkbox-link"]
+
+export const AVAILABLE_TYPES = [
+    "checkbox"
+] //["checkbox", "checkbox-link", "markdown", "h1", "h2", "h3", "p", "image", "caption"]
